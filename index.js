d63bf555d254deab677b51050ad6cd274b51507d
Vikash Kumar <75295300+Vikashrock45@users.noreply.github.com>
2021-12-07 20:39:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImage1);
+image2.setAttribute("src", randomImage2);
+
+//if player1 wins
+if(randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "🚩Player 1 WINS!!!"
+}
+
+//if player2 wins
+else if(randomNumber2 > randomNumber1) {
+	document.querySelector("h1").innerHTML = "Player 2 WINS!! 🚩"
+}
+
+//if same score
+else {
+	document.querySelector("h1").innerHTML = "Draw!!"
+}
